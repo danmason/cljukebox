5c888b447c2935abd565b6c001328a9f637d5163
Daniel Mason <dan@juxt.pro>
2021-09-27 13:30:37 +0100

    Set minor version number in project.clj.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljukebox "0.0.1"
+(defproject cljukebox "0.1.0"
   :description "A clojure based self-hosted music bot for Discord"
   :url "https://github.com/danmason/cljukebox"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
